628ccc449cd368e2f20ba9fc661f64820b22cef5
Ian Blackburn <ianblackburndeveloper@gmail.com>
2022-10-25 22:19:46 +0100

    Organize folders and convert to arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
-function Expenses(props) {
+
+const Expenses = (props) => {
 	return (
 		<Card className="expenses">
 			<ExpenseItem
@@ -26,6 +27,6 @@ function Expenses(props) {
 			></ExpenseItem>
 		</Card>
 	);
-}
+};
 
 export default Expenses;
