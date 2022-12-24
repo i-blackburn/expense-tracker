bd394e58cdca581964ff165e86c7525e194a2b26
Ian Blackburn <ianblackburndeveloper@gmail.com>
2022-12-24 11:17:17 +0000

    End of part 67 commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,19 @@ const Expenses = (props) => {
 	const filteredExpenses = props.expenses.filter((expense) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
+	let expensesContent = <p>No expenses found...</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<div>
 			<Card className="expenses">
@@ -19,14 +32,7 @@ const Expenses = (props) => {
 					selectedYear={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{filteredExpenses.map((expense) => (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				))}
+				{expensesContent}
 			</Card>
 		</div>
 	);
